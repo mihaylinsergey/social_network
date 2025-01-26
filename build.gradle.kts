d26105e142aae16fb78702ffa8c91be630579c2c
mikhailin.sa <mikhailin.sa@meket.ru>
2025-01-26 17:59:53 +0300

    adaad lost

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,17 @@ repositories {
 }
 
 subprojects {
+    apply(plugin = "java")
+
     repositories {
         mavenCentral()
     }
+
     group = rootProject.group
     version = rootProject.version
+
+    dependencies {
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    }
 }
